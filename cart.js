134ab886096d14ba3e754f61c15659ddeaaf78a4
Idris Aminat <109235073+Aminat133@users.noreply.github.com>
2022-08-28 14:49:33 +0100

    update

---
+++ cart.js
@@ -0,0 +1,11 @@
+function codeCheck() {
+    var code= document.getElementById('b').value;
+    if(code==="boss"){
+        alert("Code successful")
+    }else{
+        alert(`The code ${code} does not exist`)
+    }
+}
+function end() {
+    alert(`Oder confirmed`)
+}
